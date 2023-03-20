7b8abeed1449d0eb11e3619f682c92d4e431e2cd
Agacy junior <juniormartinssom@gmail.com>
2023-03-20 10:52:32 -0300

    Closing Swipeable via ref

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { View, ScrollView, Alert, Pressable } from "react-native";
 import { HouseLine, Trash } from "phosphor-react-native";
@@ -24,6 +24,8 @@ export function History() {
 
   const { goBack } = useNavigation();
 
+  const swipeableRef = useRef<Swipeable[]>([]);
+
   async function fetchHistory() {
     const response = await historyGetAll();
     setHistory(response);
@@ -36,7 +38,9 @@ export function History() {
     fetchHistory();
   }
 
-  function handleRemove(id: string) {
+  function handleRemove(id: string, index: number) {
+    swipeableRef.current?.[index].close();
+
     Alert.alert("Remover", "Deseja remover esse registro?", [
       {
         text: "Sim",
@@ -67,7 +71,7 @@ export function History() {
         contentContainerStyle={styles.history}
         showsVerticalScrollIndicator={false}
       >
-        {history.map((item) => (
+        {history.map((item, index) => (
           <Animated.View
             key={item.id}
             entering={SlideInRight}
@@ -75,12 +79,17 @@ export function History() {
             layout={Layout.springify()}
           >
             <Swipeable
+              ref={(ref) => {
+                if (ref) {
+                  swipeableRef.current.push(ref);
+                }
+              }}
               overshootLeft={false}
               containerStyle={styles.swipeableContainer}
               renderRightActions={() => (
                 <Pressable
                   style={styles.swipeableRemove}
-                  onPress={() => handleRemove(item.id)}
+                  onPress={() => handleRemove(item.id, index)}
                 >
                   <Trash size={32} color={THEME.COLORS.GREY_100} />
                 </Pressable>
