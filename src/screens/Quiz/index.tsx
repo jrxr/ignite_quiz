9521e72de1eb50e675ac8af0c30424917e30d192
Agacy junior <juniormartinssom@gmail.com>
2023-03-21 10:57:24 -0300

    Animating the OverlayFeedback

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -47,6 +47,8 @@ export function Quiz() {
     null
   );
 
+  const [statusReply, setStatusReply] = useState(0);
+
   const shake = useSharedValue(0);
   const scrollY = useSharedValue(0);
   const cardPosition = useSharedValue(0);
@@ -92,8 +94,10 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
+      setStatusReply(1);
       setPoints((prevState) => prevState + 1);
     } else {
+      setStatusReply(2);
       shakeAnimation();
     }
 
@@ -211,7 +215,7 @@ export function Quiz() {
 
   return (
     <View style={styles.container}>
-      <OverlayFeedback status={0} />
+      <OverlayFeedback status={statusReply} />
 
       <Animated.View style={fixedProgressBarStyles}>
         <Text style={styles.title}>{quiz.title}</Text>
